70dafa3273e87ffc0072bff0f6d11266a944d6cf
Sergey Zak <93543663+Nedravka@users.noreply.github.com>
2022-08-08 20:47:22 +0400

    Add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,13 +5,15 @@ class Pokemon(models.Model):
 
     title = models.CharField(max_length=200, blank=True)
 
-    pokemon_image = models.ImageField(upload_to='pokemon_image',null=True, blank=True)
+    pokemon_image = models.ImageField(upload_to='pokemon_image', null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
+
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
 
     longitude = models.FloatField()
     latitude = models.FloatField()
